e206b7b4c16ed8f8d4fca6e9c69c9bb7000e578e
Fabio Guelfi <user.fabioguelfiunix@gmail.com>
2017-09-19 12:42:33 -0300

    Router to keywords

--- app.js
+++ app.js
@@ -2,64 +2,7 @@ var app = require('./config/express')();
 var rotasSite = require('./app/routes/router')(app);
 var rotasKeywords = require('./app/routes/keywords')(app);
 
-
-const express = require('express');
-// All stay in Public is free to shared
-global.APP_ROOT = require('path').join(__dirname);
-app.use(express.static(APP_ROOT + '/public'));
-
-
-// Body Parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
-app.post('/send', (req,res) => {
-    const output = `
-    <h3>Instituto de Iniciaçoes Maçonicas</h3>
-    <p>#LuPercio Esta preparando
-    </p>
-   
-      <!-- EXEMPLE TO GET INPUT >> <li>Nome: ${req.body.name}</li> -->
-     
-  `;
-
-// create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
-    host: 'mail.agencia33.net',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: 'fabio@agencia33.net', // generated ethereal user
-        pass: 'abc123456'  // generated ethereal password
-    },
-    tls:{
-        rejectUnauthorized:false
-    }
-});
-
-// setup email data with unicode symbols
-let mailOptions = {
-    from: '"Nodemailer <fabio@agencia33.net>', // sender address
-    to: 'fabio@agencia33.net', // list of receivers
-    subject: 'Nodemailer', // Subject line
-    html: output // html body
-};
-
-// send mail with defined transport object
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        return console.log(error);
-    }
-    console.log('Message sent: %s', info.messageId);
-console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-res.render('contact', {msg:'E-mail enviado com sucesso'});
-});
-
-
-
-const express = require('express');
+var express = require('express');
 // All stay in Public is free to shared
 global.APP_ROOT = require('path').join(__dirname);
 app.use(express.static(APP_ROOT + '/public'));
--- app/routes/keywords.js
+++ app/routes/keywords.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-    // INDEX
+    // COMO ENTRAR NA MAÇONARIA
     app.get("/como-entrar-maconaria",function(req, res) {
         res.render('base-site/keywords/como-entrar-maconaria', {
             //Per page
@@ -20,5 +20,27 @@ module.exports = function(app) {
             author : 'Fabio Guelfi'
         });
     });
+
+    // COMO SER MACOM
+    app.get("/como-ser-macom", function (req, res) {
+        res.render("base-site/keywords/como-ser-macom", {
+            //Per page
+            title :'Como ser Maçom',
+            description : 'O postulante ao ingresso nos quadros da Ordem Maçônica, deve autoavaliar-se em busca de valores, costumes, atitudes (interiores), e  comportamentos sociais exteriorizados cotejando-os com algumas premissas...',
+            keywords : 'maconaria macom ordem sociedade fraternal integrante organizacao misteriosa sigilosa praticas segredos ritos tradicoes especulacoes reunioes cumprimento palavras socorro senha cerimonia companheiros apuros poderosos monarcas mil milhoes mundo ffinancas politica mobilizar governo controlar Winston Churchill personagens ilustres corrupcao perjurio juramento testemunhar contra violar falso processo judicial trair simbolo dolar nota triangulo olho ve novus ordo seclorum lema Benjamin Franklin design comite coincidencia iniciacao forca pescoco corda garganta simbolizar cordao umbilical',
+            //Contact
+            email : 'contato@comosermacom.com.br',
+            tel : '11 4515-7486',
+            whats : '#',
+            endereco : 'Rua Padre Benedito de Camargo 797, Penha - SP Capital',
+            cidade : 'São Paulo',
+            cep : '03604-010 ',
+            favicon : 'favicon.png',
+            linkgoogle : '',
+            linkface : '',
+            linktwitter : '',
+            author : 'Fabio Guelfi'})
+
+    })
 
 };
